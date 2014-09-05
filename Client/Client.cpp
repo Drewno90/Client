5b762a11be09b4316effadcced7fa1e88d050129
Drewno90 <dawid1990@ymail.com>
2014-09-05 20:15:24 +0200

    przesyl macierzy

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,139 +1,107 @@
-#include <stdio.h>
-
+#include <cstdio>
+#include<iostream>
 #include <winsock2.h>
-
-
-
+using namespace std;
 int main()
-
 {
-
+	int n, i, j,k;
 	// Initialize Winsock.
 
 	WSADATA wsaData;
-
 	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-
-
 	if (iResult != NO_ERROR)
-
-		printf("Client: Error at WSAStartup().\n");
-
+		cout << "Client: Error at WSAStartup()." << endl;
 	else
-
-		printf("Client: WSAStartup() is OK.\n");
-
-
-
+		cout << "Client: WSAStartup() is OK." << endl;
 	// Create a socket.
 
 	SOCKET m_socket;
 
 	m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-
-
 	if (m_socket == INVALID_SOCKET)
-
 	{
-
-		printf("Client: socket() - Error at socket(): %ld\n", WSAGetLastError());
-
+		cout << "Client: socket() - Error at socket(): " << WSAGetLastError();
 		WSACleanup();
-
 		return 0;
-
 	}
-
 	else
-
-		printf("Client: socket() is OK.\n");
-
-
+		cout << "Client: socket() is OK." << endl;
 
 	// Connect to a server.
 
 	sockaddr_in clientService;
 
-
-
 	clientService.sin_family = AF_INET;
-
 	clientService.sin_addr.s_addr = inet_addr("192.168.1.80");
-
 	clientService.sin_port = htons(55555);
 
-
-
 	if (connect(m_socket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR)
-
 	{
-
-		printf("Client: connect() - Failed to connect.\n");
+		cout << "Client: connect() - Failed to connect." << endl;
 
 		WSACleanup();
-
 		return 0;
-
 	}
-
-
-
 	// Send and receive data.
 
 	int bytesSent;
-
 	int bytesRecv = SOCKET_ERROR;
 
 	// Be careful with the array bound, provide some checking mechanism
 
-	char sendbuf[200] = "Client: Sending some test string to server...";
-
 	char recvbuf[200] = "";
 
 
+	cout << "wprowadz wymiar macierzy: ";
+	cin >> n;
+	double **A = new double *[n];
+	for (i = 0; i < n; i++)
+		A[i] = new double[n];
 
-	bytesSent = send(m_socket, sendbuf, strlen(sendbuf), 0);
+	for (i = 0; i < n; i++)
+	for (j = 0; j < n; j++)
+		cin >> A[i][j];
 
-	printf("Client: send() - Bytes Sent: %ld\n", bytesSent);
-
-
-
-	while (bytesRecv == SOCKET_ERROR)
-
-	{
-
-		bytesRecv = recv(m_socket, recvbuf, 32, 0);
-
-		if (bytesRecv == 0 || bytesRecv == WSAECONNRESET)
-
-		{
-
-			printf("Client: Connection Closed.\n");
-
-			break;
-
-		}
-
-		else
-
-			printf("Client: recv() is OK.\n");
-
-
-
-		if (bytesRecv < 0)
-
-			return 0;
-
-		else
-
-			printf("Client: Bytes received - %ld.\n", bytesRecv);
-
+	for (i = 0; i < n; i++){
+	cout << endl;
+	for (j = 0; j < n; j++)
+		cout <<" "<< A[i][j];
+}
+	cout << endl;
+	k = 0;
+	char *sendbuf=new char [n*n];
+	for (i = 0; i < n; i++)
+	for (j = 0; j < n; j++){
+		sendbuf[k] = A[i][j];
+		k++;
 	}
 
+	bytesSent = send(m_socket, sendbuf, n*n, 0);
 
+	cout<< "Client: send() - Bytes Sent:"<< bytesSent << endl;
 
+	while (bytesRecv == SOCKET_ERROR)
+	{
+		bytesRecv = recv(m_socket, recvbuf, 32, 0);
+		if (bytesRecv == 0 || bytesRecv == WSAECONNRESET)
+		{
+			cout<<"Client: Connection Closed."<<endl;
+			break;
+		}
+		else
+			cout<< "Client: recv() is OK."<<endl;
+
+		if (bytesRecv < 0)
+			return 0;
+		else
+			cout<<"Client: Bytes received - "<< bytesRecv<<endl;
+	}
+
+	bytesSent = send(m_socket,(char*) sendbuf, n*n, 0);
+	cout<<"Client: Bytes sent "<< bytesSent<<endl;
+	getchar();
+	getchar();
 	return 0;
 
 }
