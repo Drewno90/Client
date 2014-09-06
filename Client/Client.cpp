f7db2a78c61955e924bf3a5b2cd6870635daa24f
Drewno90 <dawid1990@ymail.com>
2014-09-06 17:42:44 +0200

    odbiór odwróconej macierzy

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-	int n, i, j,k;
+	int n, i, j, k;
 	// Initialize Winsock.
 
 	WSADATA wsaData;
@@ -33,7 +33,7 @@ int main()
 	sockaddr_in clientService;
 
 	clientService.sin_family = AF_INET;
-	clientService.sin_addr.s_addr = inet_addr("192.168.1.80");
+	clientService.sin_addr.s_addr = inet_addr("192.168.1.82");
 	clientService.sin_port = htons(55555);
 
 	if (connect(m_socket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR)
@@ -56,50 +56,66 @@ int main()
 	cout << "wprowadz wymiar macierzy: ";
 	cin >> n;
 	double **A = new double *[n];
+	double *B = new double[n*n];
+	double *C = new double[n*n];
 	for (i = 0; i < n; i++)
 		A[i] = new double[n];
-
-	for (i = 0; i < n; i++)
-	for (j = 0; j < n; j++)
-		cin >> A[i][j];
-
-	for (i = 0; i < n; i++){
-	cout << endl;
-	for (j = 0; j < n; j++)
-		cout <<" "<< A[i][j];
-}
-	cout << endl;
 	k = 0;
-	char *sendbuf=new char [n*n];
 	for (i = 0; i < n; i++)
 	for (j = 0; j < n; j++){
-		sendbuf[k] = A[i][j];
+		cin >> A[i][j];
+		B[k] = A[i][j];
 		k++;
 	}
+	for (i = 0; i < n; i++){
+		cout << endl;
+		for (j = 0; j < n; j++)
+			cout << " " << A[i][j];
+	}
+	cout << endl;
+	char *sendbuf = new char[n*n];
+	sendbuf[0] = n*n;
 
-	bytesSent = send(m_socket, sendbuf, n*n, 0);
-
-	cout<< "Client: send() - Bytes Sent:"<< bytesSent << endl;
+	send(m_socket, sendbuf, sizeof(int), 0);
+	for (i = 0; i < n*n; i++){
+		memcpy(sendbuf, &B[i], sizeof(double));
+		bytesSent = send(m_socket, sendbuf, sizeof(double), 0);
+	}
+	cout << "Client: send() - Bytes Sent:" << bytesSent << endl;
 
 	while (bytesRecv == SOCKET_ERROR)
 	{
 		bytesRecv = recv(m_socket, recvbuf, 32, 0);
 		if (bytesRecv == 0 || bytesRecv == WSAECONNRESET)
 		{
-			cout<<"Client: Connection Closed."<<endl;
+			cout << "Client: Connection Closed." << endl;
 			break;
 		}
 		else
-			cout<< "Client: recv() is OK."<<endl;
+			cout << "Client: recv() is OK." << endl;
 
 		if (bytesRecv < 0)
 			return 0;
 		else
-			cout<<"Client: Bytes received - "<< bytesRecv<<endl;
+			cout << "Client: Bytes received - " << bytesRecv << endl;
 	}
 
-	bytesSent = send(m_socket,(char*) sendbuf, n*n, 0);
-	cout<<"Client: Bytes sent "<< bytesSent<<endl;
+	bytesSent = send(m_socket, (char*)sendbuf, n*n, 0);
+	cout << "Client: Bytes sent " << bytesSent << endl;
+	for (int i = -1; i < n*n; i++){
+		recv(m_socket, recvbuf, sizeof(double), 0);
+		memcpy(&C[i], recvbuf, sizeof(double));
+	}
+	k = 0;
+	for (i = 0; i < n; i++){
+	cout << endl;
+	for (j = 0; j < n; j++){
+		A[i][j] = C[k];
+		cout << A[i][j] << " ";
+		k++;
+	}
+}
+
 	getchar();
 	getchar();
 	return 0;
